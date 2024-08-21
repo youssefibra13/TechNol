6706af4ccea2aeca194ba0ffab9bc626e6880dfe
youssefibra13 <72321337+youssefibra13@users.noreply.github.com>
2024-08-21 04:28:24 -0500

    Finished the Responsive design features.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/Logo.png'
 import { FaBars } from "react-icons/fa";
@@ -7,21 +7,30 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(window.innerWidth > 800 ? true : false);
+  const closeNavHandler = () => {
+    if(window.innerWidth < 800){
+      setIsNavOpen(false)
+    } else {
+      setIsNavOpen(true)
+    }
+
+  }
   return (
 
     <nav>
       <div className="container nav_container">
-        <Link to="/" className="nav_logo">
+        <Link to="/" className="nav_logo" onClick={closeNavHandler}>
           <img src={Logo} alt="Navbar Logo" />
         </Link>
-        <ul className="nav_links">
-          <li><Link to="/profile"> Profile</Link></li>
-          <li><Link to="/Authors">Authors</Link></li>
-          <li><Link to="/create">Create Post</Link></li>
-          <li><Link to="/logout">Logout</Link></li>
-        </ul>
-        <button className="nav_toggle-btn">
-          <AiOutlineCloseCircle />
+        {isNavOpen && <ul className="nav_links">
+          <li><Link to="/profile" onClick={closeNavHandler}>  Profile</Link></li>
+          <li><Link to="/Authors" onClick={closeNavHandler}>Authors</Link></li>
+          <li><Link to="/create" onClick={closeNavHandler}>Create Post</Link></li>
+          <li><Link to="/logout" onClick={closeNavHandler}>Logout</Link></li>
+        </ul>}
+        <button className="nav_toggle-btn" onClick={() => setIsNavOpen(!isNavOpen)}>
+          {isNavOpen ? <AiOutlineCloseCircle /> : <FaBars />}
 
 
         </button>
